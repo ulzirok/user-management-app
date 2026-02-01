95a239de791fb9ffee5f8b76454a204faa28e7b3
ulzirok <ulzirok@gmail.com>
2026-02-01 23:54:01 +0500

    change baseURL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instanceAxios = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 });
 
 instanceAxios.interceptors.request.use((config) => {
