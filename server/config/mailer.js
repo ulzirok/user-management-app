992e7be651be1fe5512c690291346f5ed70e3cac
ulzirok <ulzirok@gmail.com>
2026-02-03 12:41:59 +0500

    fix: change the mailer logic

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,11 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = transporter;
